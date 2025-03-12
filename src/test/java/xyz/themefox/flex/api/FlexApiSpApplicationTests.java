7874578ac275d1ca2600528d41a78ffbd85808c7
ningwangsee <ning.wang.see@gmail.com>
2025-03-12 14:24:15 -0700

    first commit

---
+++ src/test/java/xyz/themefox/flex/api/FlexApiSpApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.themefox.flex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexApiSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
